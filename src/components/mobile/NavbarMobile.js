4720e33f9182265ff0996fa5915d76f3ac4fe6df
unknown <onerofad>
2025-09-11 17:24:53 +0100

    first commit

--- src/components/mobile/NavbarMobile.js
+++ src/components/mobile/NavbarMobile.js
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router"
-import { Menu, Button, Image } from "semantic-ui-react"
+import { Menu, Button, Image, Grid } from "semantic-ui-react"
 import logo from '../../images/icon.png'
 
 const NavbarMobile = () => {
@@ -7,7 +7,9 @@ const NavbarMobile = () => {
      const navigate = useNavigate()
 
     return(
-    <Menu secondary>
+    <Grid>
+        <Grid.Column>
+<Menu secondary>
           <Menu.Item>
             <Image
                src={logo} style={{width: 30, height: 30}}
@@ -21,6 +23,7 @@ const NavbarMobile = () => {
             <Button 
               onClick={() => navigate('/login')} 
               color="blue" 
+              size="small"
               style={{
                 fontStyle: 'normal',
                 fontWeight: 400,
@@ -32,6 +35,9 @@ const NavbarMobile = () => {
             </Button>
          </Menu.Item>
       </Menu>
+        </Grid.Column>
+    </Grid>
+    
     )
 }
 export default NavbarMobile

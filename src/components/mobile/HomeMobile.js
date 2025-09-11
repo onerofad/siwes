1e7929c19d59853b6554b778f24721c214f30039
unknown <onerofad>
2025-09-11 17:37:09 +0100

    first commit

--- src/components/mobile/HomeMobile.js
+++ src/components/mobile/HomeMobile.js
@@ -10,18 +10,35 @@ import WallPaperMobile from "./WallPaperMobile"
 const HomeMobile = () => {
 
         return(
-            <>
             <Grid>
-                <Grid.Column>
-                    <NavbarMobile />
-                    <WallPaperMobile />
-                    <AboutMobile />
-                    <ObjectivesMobile />
-                    <FooterMobile />
-                    </Grid.Column>
+               <Grid.Column>
+                <Menu secondary>
+                        <Menu.Item>
+                            <Image
+                            src={logo} style={{width: 30, height: 30}}
+                            />
+                            <span style={{display: 'inline', color: '#2980b9', fontFamily: 'Inter', fontSize: 13, fontWeight: 800, fontStyle: 'normal', verticalAlign: 'middle'}}>
+                                DELSU SIWES PORTAL
+                            </span>
+                        </Menu.Item>
+                        <Menu.Item position='right'>
+                            <Button 
+                            onClick={() => navigate('/login')} 
+                            color="blue" 
+                            size="small"
+                            style={{
+                                fontStyle: 'normal',
+                                fontWeight: 400,
+                                fontSize: 16,
+                                fontFamily: 'Prompt'
+                            }}
+                            >
+                            Login
+                            </Button>
+                        </Menu.Item>
+                    </Menu>
+                </Grid.Column>
             </Grid>
-               
-            </>
         )
 }
   

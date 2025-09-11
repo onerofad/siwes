890c198482c6c203c34e4c3120ad3ead19d7a1d2
unknown <onerofad>
2025-09-11 21:03:36 +0100

    first commit

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -52,6 +52,109 @@ const Dashboard = ({mobile}) => {
                        <DashboardMenu mobile={mobile} />
                     </Grid.Column>
                 </Grid.Row>
+                 <Grid.Row>
+                    <Grid.Column width={16}  style={{marginTop: 40, padding: '0px 10px', height: '90vh'}}>
+                            <Sidebar.Pushable as={Segment} >
+                                <Button secondary size="large" icon onClick={() => setVisible(!visible)}>
+                                    <Icon name='sidebar' />
+                                </Button>
+                                <Sidebar 
+                                    as={Menu}
+                                    visible={visible}
+                                    inverted
+                                    vertical
+                                    width="thin"
+                                    onHide={() => setVisible(false)}
+                                    animation="overlay"
+                                   
+                                >
+                              
+                                              <Menu.Item as='a'>
+                                    <Image circular centered src={logo} />
+                                    <Header inverted textAlign="center">Student Menu</Header>
+                                </Menu.Item>
+                                <Menu.Item 
+                                    as='a'
+                                    active={activeItem === "dashboard"}
+                                    onClick={() => setactiveITem("dashboard")}
+                                >
+                                    <Header inverted as="h5">
+                                        <Icon name='clock outline' />
+                                        <Header.Content>Get Started</Header.Content>
+                                    </Header>
+                                </Menu.Item>
+                                <Menu.Item
+                                    as='a'
+                                    active={activeItem === "profile"}
+                                    onClick={() => setactiveITem("profile")}
+                        
+                                >
+                                    <Header inverted as="h5">
+                                        <Icon name='user outline' />
+                                        <Header.Content>My Profile</Header.Content>
+                                    </Header>
+                                </Menu.Item>
+                                <Menu.Item 
+                                    as='a'
+                                    active={activeItem === "siwes"}
+                                    onClick={() => { setactiveITem("siwes")}} 
+                                >
+                                    <Header inverted as="h5">
+                                        <Icon name='university' />
+                                        <Header.Content>Siwes</Header.Content>
+                                    </Header>
+                                </Menu.Item>
+                                <Menu.Item 
+                                    as='a'
+                                    active={activeItem === "location"}
+                                    onClick={() => { setactiveITem("location")}} 
+                                >
+                                    <Header inverted as="h5">
+                                        <Icon name='map outline' />
+                                        <Header.Content>Location Payment</Header.Content>
+                                    </Header>
+                                </Menu.Item>
+                                <Menu.Item 
+                                    as='a'
+                                    active={activeItem === "payment"}
+                                    onClick={() => { setactiveITem("payment")}} 
+                                >
+                                    <Header inverted as="h5">
+                                        <Icon name='money bill alternate outline' />
+                                        <Header.Content>Payments</Header.Content>
+                                    </Header>
+                                </Menu.Item>
+                                <Menu.Item 
+                                    as='a'
+                                    onClick={() => {navigate("/login")}} 
+                                >
+                                <Header inverted as="h5">
+                                    <Icon name='sign-out' />
+                                    <Header.Content>Log out</Header.Content>
+                                </Header>
+                                </Menu.Item>
+                                </Sidebar>
+                                <Sidebar.Pusher style={{padding: '0px 20px'}}>
+                                    {
+                                        activeItem === "dashboard" && <DashboardHome />
+                                    }
+                                    {
+                                        activeItem === "profile" && <DashboardProfile />
+                                    }
+                                    {
+                                        activeItem === "siwes" && <DashboardSiwes />
+                                    }
+                                    {
+                                        activeItem === "location" && <DashboardLocation />
+                                    }
+                                    {
+                                        activeItem === "payment" && <DashboardPayment />
+                                    }
+                                </Sidebar.Pusher>
+
+                            </Sidebar.Pushable>
+                        </Grid.Column>
+                    </Grid.Row>
           
             </Grid>
         )

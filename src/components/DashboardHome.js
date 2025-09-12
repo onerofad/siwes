00cebb30fafb048fb36ee99abed4e6308f01f5db
unknown <onerofad>
2025-09-12 11:05:00 +0100

    first commit

--- src/components/DashboardHome.js
+++ src/components/DashboardHome.js
@@ -1,4 +1,4 @@
-import { Grid, Header, Segment , Label, Message} from "semantic-ui-react";
+import { Grid, Header, Segment , Label, Message, List} from "semantic-ui-react";
 import '../css/style.css'
 
 
@@ -8,7 +8,7 @@ const DashboardHome = ({mobile}) => (
         <Label style={{marginTop: mobile ? 20 : 70, marginLeft: 30, marginBottom: 20}} size="large" ribbon color="blue">GUIDELINES</Label>                         
           
             <Message negative size={mobile ? 'mini' : 'large'}>
-                <p class="ribbon">
+                {/*<p class="ribbon">
                     1. Click on My Profile menu to view profile
                 </p>
                 <p class="ribbon">
@@ -19,7 +19,33 @@ const DashboardHome = ({mobile}) => (
                 </p>  
                 <p class="ribbon" style={{display: 'block'}}>
                     4. Meet your department SIWES Coordinator/SIWES Directorate for further instructions	
-                </p> 
+                </p> */}
+                <List relaxed>
+                    <List.Item style={{color: '#000'}}>
+                        <List.Icon name="arrow alternate circle right" />
+                        <List.Content>
+                            Click on My Profile menu to view profile
+                        </List.Content>
+                    </List.Item>
+                    <List.Item style={{color: '#000'}}>
+                        <List.Icon name="arrow alternate circle right" />
+                        <List.Content>
+                            Click on Siwes menu to update your SIWES details
+                        </List.Content>
+                    </List.Item>
+                    <List.Item style={{color: '#000'}}>
+                        <List.Icon name="arrow alternate circle right" />
+                        <List.Content>
+                            Proceed to Location menu to select SIWES location and make payment
+                        </List.Content>
+                    </List.Item>
+                    <List.Item style={{color: '#000'}}>
+                        <List.Icon name="arrow alternate circle right" />
+                        <List.Content>
+                            Meet your department SIWES Coordinator/SIWES Directorate for further instructions
+                        </List.Content>
+                    </List.Item>
+                </List>
             </Message>
 
     </div>

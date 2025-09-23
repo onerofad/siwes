248c22002bfef2135b024fa0e9601b3438c6dfd1
unknown <onerofad>
2025-09-23 13:43:20 +0100

    first commit

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -70,7 +70,7 @@ const Dashboard = ({mobile}) => {
                         pointing
                     >
                         <Menu.Item>
-                            <Image circular centered src={logo} />
+                            <Image size="small" circular centered src={logo} />
                             <Header as="h5" inverted textAlign="center">
                                 Student Menu
                             </Header>
